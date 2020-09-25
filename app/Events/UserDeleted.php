8440d5754e0670f440cf077019ebbf9f64a2f1b7
davidcrc <david_drcr@hotmail.com>
2020-09-25 11:26:00 -0500

    P5-V3 Creando los eventos para indicar los cambios de sesion en los usuarios

---
+++ app/Events/UserDeleted.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+// Añadir: implements ShouldBroadcast
+class UserDeleted implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    // ACa inicializamos todo lo q deseamos enviar
+    public $user;
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        //
+        $this->user = $user;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    // Modificar 
+    public function broadcastOn()
+    {
+        // return new PrivateChannel('channel-name');
+        // \Log::debug("{$this->message}");
+        // \Log::debug("{$this->type}");
+        return new Channel('users');
+    }
+}
