7cfac7043816f17210bc664afc97c931c1108060
davidcrc <david_drcr@hotmail.com>
2020-09-25 12:16:55 -0500

    P5-V5 Escuchando el canal 'user' atraves de laravel Echo

--- resources/views/users/showAll.blade.php
+++ resources/views/users/showAll.blade.php
@@ -23,8 +23,8 @@
         
         // console.log(window)
         window.axios.get('/api/users').then((response) => {
-                console.log(response)
-                // const usersElement = document.getElementById('users');
+                // console.log(response)
+                const usersElement = document.getElementById('users');
                 let users = response.data;
                 users.forEach((user, index) => {
                     let element = document.createElement('li');
@@ -32,6 +32,25 @@
                     element.innerText = user.name;
                     usersElement.appendChild(element);
                 });
+            });
+    </script>
+
+    <script>
+        Echo.channel('users')
+            .listen('UserCreated', (e) => {
+                const usersElement = document.getElementById('users');
+                let element = document.createElement('li');
+                element.setAttribute('id', e.user.id);
+                element.innerText = e.user.name;
+                usersElement.appendChild(element);
+            })
+            .listen('UserUpdated', (e) => {
+                let element = document.getElementById(e.user.id);
+                element.innerText = e.user.name;
+            })
+            .listen('UserDeleted', (e) => {
+                let element = document.getElementById(e.user.id);
+                element.parentNode.removeChild(element);
             });
     </script>
 @endpush
